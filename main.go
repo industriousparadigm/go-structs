438bf498ae286c8fd00ac8eb7fe4c4a653561d9a
mrsalvini <dsgmcosta@gmail.com>
2019-08-13 13:33:30 +0100

    memory tests

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Kimberly",
+		contactInfo: contactInfo{
+			email:   "jim@jimail.com",
+			zipCode: 80656,
+		},
+	}
+
+	jim.updateName("rambo")
+	jim.print()
+}
+
+func (p *person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
